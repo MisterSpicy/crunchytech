ef24b7866f7a9f5452f5f4339ff94b352b3b13a4
FrostRocket <mjgroves182@gmail.com>
2014-09-06 18:31:34 -0700

    Remove breeezzeeeeee

--- Breezeee/src/com/crunchytech/breeze/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.crunchytech.breeze;
-
-public final class Constants {
-
-	public static final int NOTIFICATION_ID = 69;
-	
-	public static final String REGISTERED = "registered";
-	public static final String DRAWER_ITEM = "drawer_item";
-}
