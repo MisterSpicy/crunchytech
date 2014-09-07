703d1d1eab90e3857cf6557e8f66a89a31dd9dc7
Gregory Enriquez <gregoryenriquez@gmail.com>
2014-09-07 08:25:58 -0700

    remove blank screen

--- breeze/src/com/crunchytech/breeze/LinkedInProfileViewer.java
+++ breeze/src/com/crunchytech/breeze/LinkedInProfileViewer.java
@@ -3,7 +3,9 @@ package com.crunchytech.breeze;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class LinkedInProfileViewer extends Activity {
 
@@ -19,6 +21,7 @@ public class LinkedInProfileViewer extends Activity {
 		WebView webview = new WebView(this);
 		setContentView(webview);
 		webview.loadUrl(publicProfileUrl);
+		finish();
 	}
 
 }
