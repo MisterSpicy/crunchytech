9555fcf8b7c4b6f42953a904d7a2e7b400c9f91c
Brian Albright <albright.brian@gmail.com>
2014-09-07 03:25:31 -0700

    Adding Volley Singleton stuff

--- breeze/src/com/crunchytech/breeze/Breeze.java
+++ breeze/src/com/crunchytech/breeze/Breeze.java
@@ -2,11 +2,20 @@ package com.crunchytech.breeze;
 
 import android.app.Application;
 import android.content.Context;
+import android.text.TextUtils;
+
+import static com.crunchytech.breeze.Constants.TAG;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class Breeze extends Application {
 	
 	private static Breeze mInstance;
     private static Context mAppContext;
+    
+    private RequestQueue mRequestQueue;
 
     @Override
     public void onCreate() {
@@ -15,9 +24,33 @@ public class Breeze extends Application {
         
         this.setAppContext(getApplicationContext());
     }
-
-    public static Breeze getInstance() {
+    
+    public static synchronized Breeze getInstance() {
         return mInstance;
+    }
+    
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+    
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+ 
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+ 
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
     }
 
     public static Context getAppContext() {
@@ -25,6 +58,6 @@ public class Breeze extends Application {
     }
 
     public void setAppContext(Context mAppContext) {
-        this.mAppContext = mAppContext;
+        Breeze.mAppContext = mAppContext;
     }
 }
--- breeze/src/com/crunchytech/breeze/server/ServerApi.java
+++ breeze/src/com/crunchytech/breeze/server/ServerApi.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 
 import android.util.Log;
 
-import com.android.volley.Request;
 import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
