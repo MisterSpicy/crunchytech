57197deb1518570229a28a78497abeb5db105cc1
Brian Albright <albright.brian@gmail.com>
2014-09-07 02:59:34 -0700

    Adding first server util stuff

---
+++ breeze/src/com/crunchytech/breeze/server/ServerApi.java
@@ -0,0 +1,18 @@
+package com.crunchytech.breeze.server;
+import static com.crunchytech.breeze.Constants.TAG;
+import android.util.Log;
+
+public class ServerApi {
+	static String serverURL = "http://107.170.245.92:5000/"
+	static String register = "register";
+	static String getnearby = "getnearby";
+	
+	
+	public ServerApi(){
+		Log.i(TAG, "Instantiate Server Api?");
+	}
+	
+	public static void sendRegistration(String name, String id, String purl){
+		
+	}
+}
