d23550e4e0ad0780cfe2b71bc43979f2bafac482
Brian Albright <albright.brian@gmail.com>
2014-09-20 00:00:32 -0700

    Adding some more appengine stuff

--- breeze-server/breeze_server.py
+++ breeze-server/breeze_server.py
@@ -1,6 +1,10 @@
 from google.appengine.api import users
+from google.appengine.ext import ndb
+
 
 import webapp2
+import cgi
+import urllib
 
 MAIN_PAGE_HTML = """\
 <html>
@@ -11,16 +15,23 @@ MAIN_PAGE_HTML = """\
 
 """
 
-class BreezeModel():
-	"""
-	Database Model
-	"""
+USER_DB = 'user_db'
 
-	#Name
-	#Identifier
-	#ProfileURL
-	#picURL
-	#Headline
+def user_db_key(user_db=USER_DB):
+	"""Create a key for the datastore to use"""
+	return ndb.key('users', user_db)
+
+def readFromDB():
+	print "Read from the DB?"
+
+class UserEntry(ndb.Model):
+	"""Model Object for individual entry"""
+
+	name = ndb.StringProperty()
+	identifier = ndb.StringProperty()
+	#profileurl = ndb.StringProperty()
+	#headline = ndb.StringProperty()
+	#picurl = ndb.StringProperty()
 
 
 class BreezeMain(webapp2.RequestHandler):
@@ -32,6 +43,20 @@ class BreezeMain(webapp2.RequestHandler):
 	def post(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		self.response.write('POST World')
+		name = self.request.get('name')
+		identifier = self.request.get('id')
+
+		# Creates a new model object for the datastore
+		user_page = UserEntry(parent=user_db_key())
+
+		user_page.name = name
+		user_page.identifier = identifier
+
+		#Store the user db values to the database
+		user_page.put()
+		# self.redirect?  Naw
+
+
 
 
 class Register(webapp2.RequestHandler):
@@ -44,7 +69,7 @@ class Register(webapp2.RequestHandler):
 class GetNearby(webapp2.RequestHandler):
 	def get(self):
 		self.response.write(MAIN_PAGE_HTML)
-		
+
 	def post(self):
 
 
